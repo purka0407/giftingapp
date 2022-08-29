4b7ae40a40f36374c0668a7b2a3c85d0c61fda8f
Aasique06 <aasique.ahmed@brainiuminfotech.com>
2022-08-29 10:52:12 +0530

    web service

---
+++ src/app/shared/web.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import{HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { map,retry,catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebService {
+
+  constructor(public httpclient: HttpClient) { }
+  getdata():Observable<any>{
+    return this.httpclient.get('https://reqres.in/api/products/3').pipe(
+      map(res => {
+          return res;
+      }),
+      retry(1),
+      );
+  }
+  getpostdata(data:any):Observable<any>{
+    return this.httpclient.post('https://reqres.in/api/products/3',data).pipe(
+      map(res => {
+          return res;
+      }),
+      retry(1),
+      );
+  }
+}
