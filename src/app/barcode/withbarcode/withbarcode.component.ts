ff34c8464cb7cfaffb6801375173ddbdd29ba6bd
Aasique06 <aasique.ahmed@brainiuminfotech.com>
2022-08-29 10:40:30 +0530

    class on 26.08.2022

--- src/app/barcode/withbarcode/withbarcode.component.ts
+++ src/app/barcode/withbarcode/withbarcode.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { WebService } from 'src/app/shared/web.service';
 
 @Component({
   selector: 'app-withbarcode',
   templateUrl: './withbarcode.component.html',
-  styleUrls: ['./withbarcode.component.scss']
+  styleUrls: ['./withbarcode.component.scss'],
 })
 export class WithbarcodeComponent implements OnInit {
-
-  constructor() { }
+  constructor(public WebService: WebService) {}
 
   ngOnInit(): void {
+    this.WebService.getdata().subscribe((res) => {
+      console.log(res);
+    });
+    this.postdata();
   }
 
+  postdata() {
+    this.WebService.getpostdata('string').subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
