f0e7a2fa9bbf3caaa430329bac0ae19942f4fa40
purka0407 <rahul.purkait@brainiuminfotech.com>
2022-08-29 10:54:13 +0530

    lazy loading 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { WithbarcodeComponent } from './barcode/withbarcode/withbarcode.component';
+
 
 
 const routes: Routes = [
